b6d0fec2c4d6039a57e10087fae4bc8104e732eb
dharmendra7 <85740691+dharmendra7@users.noreply.github.com>
2023-10-06 12:53:31 +0530

    changes in credentials settings.py

--- taskmanagement/settings.py
+++ taskmanagement/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "dmrathod8080@gmail.com"
-EMAIL_HOST_PASSWORD = "korg lyyo yqno tmni"
+EMAIL_HOST_USER = "Your host email"
+EMAIL_HOST_PASSWORD = "Your host Password"
